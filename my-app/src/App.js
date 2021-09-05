311ca1f2d0fe607b6fd856ba3d6048e7cf99068d
jrtwheeler <jwheele1@gmail.com>
2021-09-04 22:35:43 -0400

    Added states for score keeping

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 const App = () => {
   const [word, setWord] = useState(Words);
   const [newWord, setNewWord] = useState(word[0]);
-  const [disabled, setDisabled] = useState(true)
+  const [disabled, setDisabled] = useState(true);
+  const [currentResults, setCurrentResults] = useState([]);
+  const [wrongResults, setWrongResults] = useState([]);
+  const [countCorrect, setCountCorrect] = useState(0);
   return (
     <div className="App">
       <h1>Welcome</h1>
