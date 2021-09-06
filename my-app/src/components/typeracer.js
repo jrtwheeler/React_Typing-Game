e3ff6e0194f9894b78d78b662d9647c10f143fec
jrtwheeler <jwheele1@gmail.com>
2021-09-05 21:09:52 -0400

    Added a button to typeracer

--- my-app/src/components/typeracer.js
+++ my-app/src/components/typeracer.js
@@ -1,11 +1,18 @@
 import React from "react";
-import button from "./button"
+import Button from "./button";
 
 const typeracer = () => {
     return (
         <div className='typeracer'>
             <div className="wordOutput">
                 <p>New Word</p>
+            </div>
+            <div className="time">
+                <p>Time</p>
+            </div>
+            <div className="wordValues">
+                <input type="text"/>
+                <Button/>
             </div>
         </div>
     )
